93d840c2835df266b093417e1d884fee9cd76c9d
Paul MacNichol <paulmacnichol@gmail.com>
2025-04-25 13:04:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
@@ -7,13 +6,16 @@ setup(
     author="Paul MacNichol",
     author_email="paul.macnichol@qatchtech.com",
     description="VisQ.AI core package",
-    # <-- look for packages in visQAI/src, not src
     packages=find_packages(where="visQAI/src"),
     package_dir={"": "visQAI/src"},
+    python_requires=">=3.8",
     install_requires=[
         "joblib>=1.2.0",
         "pandas>=1.5.0",
         "PyQt5>=5.15.0",
+        "pysqlcipher3>=1.0.3",
+        "tensorflow>=2.10.0",
+        "optuna>=3.0.0",
+        "matplotlib>=3.6.0",
     ],
-    python_requires=">=3.8",
 )
