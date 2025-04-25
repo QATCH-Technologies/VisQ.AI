e966cbf7e37b4c87687f09b8c04d07442c47783d
Paul MacNichol <paulmacnichol@gmail.com>
2025-04-25 13:12:23 -0400

    Update __init__.py

--- visQAI/src/trainers/__init__.py
+++ visQAI/src/trainers/__init__.py
@@ -3,4 +3,4 @@
 from .cnn_trainer import ViscosityTrainerCNN
 from .linear_trainer import ViscosityRegressorLM
 from .nn_trainer import XGBTrainer
-from .xgb_trainer import XGBTrainer
+# from .xgb_trainer import XGBTrainer
