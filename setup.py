f52d2a4d784f8369b0672914649d640c8a860da5
Paul MacNichol <paulmacnichol@gmail.com>
2025-04-25 13:09:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,16 @@ setup(
     package_dir={"": "visQAI/src"},
     python_requires=">=3.8",
     install_requires=[
+        # Core
         "joblib>=1.2.0",
         "pandas>=1.5.0",
         "PyQt5>=5.15.0",
+        # ML & plotting
         "tensorflow>=2.10.0",
         "optuna>=3.0.0",
         "matplotlib>=3.6.0",
+        # Additional model libs
+        "xgboost>=1.7.0",
+        "scikit-learn>=1.0.0",
     ],
 )
