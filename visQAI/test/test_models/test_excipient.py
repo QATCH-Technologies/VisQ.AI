adeb87dbca5b92ea89aa68d5195f91daf8e30b12
Paul MacNichol <paulmacnichol@gmail.com>
2025-05-01 09:53:16 -0400

    updating excipients, controller, db, and app.py to work with new definition

--- visQAI/test/test_models/test_excipient.py
+++ visQAI/test/test_models/test_excipient.py
@@ -22,31 +22,45 @@ class TestConcentrationUnit(unittest.TestCase):
 class TestBaseExcipient(unittest.TestCase):
     def test_equality_and_hash(self):
         uid = uuid.uuid4()
-        e1 = BaseExcipient(name="Test", id=uid)
-        e2 = BaseExcipient(name="Other", id=uid)
+        e1 = BaseExcipient(name="Test", etype="Generic", id=uid)
+        e2 = BaseExcipient(name="Other", etype="Generic", id=uid)
         self.assertEqual(e1, e2)
         self.assertEqual(hash(e1), hash(e2))
-        e3 = BaseExcipient(name="Test", id=uuid.uuid4())
+        e3 = BaseExcipient(name="Test", etype="Generic", id=uuid.uuid4())
         self.assertNotEqual(e1, e3)
 
     def test_not_equal_to_other_types(self):
-        e = BaseExcipient(name="X")
+        e = BaseExcipient(name="X", etype="Generic")
         self.assertFalse(e == "X")
         self.assertNotEqual(e, 123)
 
     def test_name_setter_valid(self):
-        e = BaseExcipient(name="Orig")
+        e = BaseExcipient(name="Orig", etype="Generic")
         e.name = " NewName "
         self.assertEqual(e.name, "NewName")
 
     def test_name_setter_invalid(self):
-        e = BaseExcipient(name="Test")
+        e = BaseExcipient(name="Test", etype="Generic")
         with self.assertRaises(ValueError):
             e.name = ""
         with self.assertRaises(ValueError):
             e.name = " "
         with self.assertRaises(TypeError):
             e.name = None
+
+    def test_etype_setter_valid(self):
+        e = BaseExcipient(name="Name", etype="Type")
+        e.etype = " NewType "
+        self.assertEqual(e.etype, "NewType")
+
+    def test_etype_setter_invalid(self):
+        e = BaseExcipient(name="Name", etype="Type")
+        with self.assertRaises(ValueError):
+            e.etype = ""
+        with self.assertRaises(ValueError):
+            e.etype = "  "
+        with self.assertRaises(TypeError):
+            e.etype = None
 
 
 class TestVisQExcipient(unittest.TestCase):
@@ -67,7 +81,7 @@ class TestVisQExcipient(unittest.TestCase):
     def test_not_equal_to_base(self):
         v = VisQExcipient(name="Buffer", etype="Buffer",
                           concentration=0.1, unit=ConcentrationUnit.PERCENT_W_V)
-        b = BaseExcipient(name="Buffer")
+        b = BaseExcipient(name="Buffer", etype="Buffer")
         self.assertNotEqual(v, b)
 
     def test_setters_valid(self):
@@ -99,12 +113,12 @@ class TestVisQExcipient(unittest.TestCase):
 
 class TestExcipientProfile(unittest.TestCase):
     def setUp(self):
-        self.base = BaseExcipient(name="Buffer")
+        self.base = BaseExcipient(name="Buffer", etype="Buffer")
         self.profile = ExcipientProfile(self.base)
 
     def test_add_and_get_variations(self):
         self.profile.add_variation(
-            etype="Buffer", concentration=0.5, unit=ConcentrationUnit.PERCENT_W_V
+            concentration=0.5, unit=ConcentrationUnit.PERCENT_W_V
         )
         vars = self.profile.get_variations()
         self.assertEqual(len(vars), 1)
@@ -116,32 +130,32 @@ class TestExcipientProfile(unittest.TestCase):
 
     def test_duplicate_variation_raises(self):
         self.profile.add_variation(
-            etype="Buffer", concentration=1.0, unit=ConcentrationUnit.MILLIGRAM_PER_ML
+            concentration=1.0, unit=ConcentrationUnit.MILLIGRAM_PER_ML
         )
         with self.assertRaises(ValueError):
             self.profile.add_variation(
-                etype="Buffer", concentration=1.0, unit=ConcentrationUnit.MILLIGRAM_PER_ML
+                concentration=1.0, unit=ConcentrationUnit.MILLIGRAM_PER_ML
             )
 
     def test_distinct_units_allowed(self):
         self.profile.add_variation(
-            etype="Buffer", concentration=1.0, unit=ConcentrationUnit.PERCENT_W_V
+            concentration=1.0, unit=ConcentrationUnit.PERCENT_W_V
         )
         self.profile.add_variation(
-            etype="Buffer", concentration=1.0, unit=ConcentrationUnit.PERCENT_V_V
+            concentration=1.0, unit=ConcentrationUnit.PERCENT_V_V
         )
         self.assertEqual(len(self.profile.get_variations()), 2)
 
     def test_variations_return_copy(self):
         self.profile.add_variation(
-            etype="Salt", concentration=0.2, unit=ConcentrationUnit.MILLIMOLAR
+            concentration=0.2, unit=ConcentrationUnit.MILLIMOLAR
         )
         vars1 = self.profile.get_variations()
         vars1.append(None)
         self.assertNotIn(None, self.profile.get_variations())
 
     def test_base_excipient_setter_valid(self):
-        new_base = BaseExcipient(name="NewBase")
+        new_base = BaseExcipient(name="NewBase", etype="NewBase")
         self.profile.base_excipient = new_base
         self.assertIs(self.profile.base_excipient, new_base)
 
